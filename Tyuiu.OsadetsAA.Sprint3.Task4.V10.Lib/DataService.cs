9f7ab8d336c15aff651a140fd1b6686c09a6ed6c
Tyuiu.OsadetsAA@outlook.com <Tyuiu.OsadetsAA@outlook.com>
2024-10-31 00:55:58 +0500

    Task4

---
+++ Tyuiu.OsadetsAA.Sprint3.Task4.V10.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.OsadetsAA.Sprint3.Task4.V10.Lib
+{
+    public class DataService : ISprint3Task4V10
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            int x;
+            double sp = 1;
+            for (x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    continue;
+                }
+                sp = sp * ((Math.Sin(x) - x) / x);
+            }
+            return Math.Round(sp, 3);
+        }
+    }
+}
---
+++ Tyuiu.OsadetsAA.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.OsadetsAA.Sprint3.Task4.V10.Lib;
+namespace Tyuiu.OsadetsAA.Sprint3.Task4.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = ("Спринт #3 | Выполнил: Осадец А. А. | ПКТб-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Осадец Александр Александрович | ПКТб-24-1                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,                        *");
+            Console.WriteLine("* вычислить значение функции y=(sin(x)-x)/x.                              *");
+            Console.WriteLine("* При х = 0 пропустить значение Полученные значения перемножать.          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(res);
+        }
+    }
+}
